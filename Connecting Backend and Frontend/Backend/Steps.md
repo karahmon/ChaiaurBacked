d18d6ab15315a2501486c4f5cac41fc0ff17ae17
karahmon <monilkarania@gmail.com>
2024-04-11 00:38:06 +0530

    third commit

---
+++ Connecting Backend and Frontend/Backend/Steps.md
@@ -0,0 +1,6 @@
+initialise using npm init 
+put start and type as module in package-lock
+create js file
+install express
+using import express syntax
+put env variables
---
+++ Connecting Backend and Frontend/Backend/server.js
@@ -0,0 +1,14 @@
+import express from 'express'
+
+const app = express();
+
+app.get('/',(req,res)=>{
+    res.send("server is ready");
+})
+app.get('/jokes',(req,res)=>{
+    const jokes=[{id: 1,title:"A joke",content:"Hello"},{id: 2,title:"Another Joke",content:"JSCA"},{id: 3,title:"Third Joke",content:"Namaskar"},{id: 4,title:"Fourth Joke",content:"Star"},{id: 5 ,title:"Fifth Joke",content:"Moon"}];
+    res.send(jokes);
+})
+const port = process.env.PORT || 3000;
+
+app.listen(port,()=>{console.log(`serve at http://localhost:${port}`)})
