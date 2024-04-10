50c5ff3d9aca2bcacefa3d57abdef45591dc2f42
karahmon <monilkarania@gmail.com>
2024-04-11 00:03:02 +0530

    first commit

--- index.js
+++ index.js
@@ -5,6 +5,41 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const github={
+    "login": "appwrite",
+    "id": 25003669,
+    "node_id": "MDEyOk9yZ2FuaXphdGlvbjI1MDAzNjY5",
+    "avatar_url": "https://avatars.githubusercontent.com/u/25003669?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/appwrite",
+    "html_url": "https://github.com/appwrite",
+    "followers_url": "https://api.github.com/users/appwrite/followers",
+    "following_url": "https://api.github.com/users/appwrite/following{/other_user}",
+    "gists_url": "https://api.github.com/users/appwrite/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/appwrite/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/appwrite/subscriptions",
+    "organizations_url": "https://api.github.com/users/appwrite/orgs",
+    "repos_url": "https://api.github.com/users/appwrite/repos",
+    "events_url": "https://api.github.com/users/appwrite/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/appwrite/received_events",
+    "type": "Organization",
+    "site_admin": false,
+    "name": "Appwrite",
+    "company": null,
+    "blog": "https://appwrite.io",
+    "location": "Planet Earth, Milky Way",
+    "email": null,
+    "hireable": null,
+    "bio": "End to end backend server for frontend and mobile developers. 👩‍💻👨‍💻",
+    "twitter_username": "appwrite",
+    "public_repos": 104,
+    "public_gists": 0,
+    "followers": 2924,
+    "following": 0,
+    "created_at": "2017-01-09T09:05:24Z",
+    "updated_at": "2023-09-28T15:24:35Z"
+  }
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -17,6 +52,9 @@ app.get('/login',(req,res)=>{
 })
 app.get('/youtube',(req,res)=>{
     res.send("<h2>@monilkarania</h2>")
+})
+app.get('/github',(req,res)=>{
+    res.json(github)
 })
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`)
