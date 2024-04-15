682e8ab084de396c38affb8c5360591b31218db6
karahmon <monilkarania@gmail.com>
2024-04-16 00:02:25 +0530

    user controller part 1 done

--- vidplay/src/controllers/user.controller.js
+++ vidplay/src/controllers/user.controller.js
@@ -1,8 +1,29 @@
 import {asyncHandler} from '../utils/asyncHandler.js';
+import {apiError} from '../utils/apiError.js'
+import {User} from '../models/user.model.js';
 
 const registerUser= asyncHandler(async (req, res) => {
-    res.status(200).json({message: 'User registered successfully'})
+   // get user details from frontend
+   // validation - not empty
+   // check if user exists from bith username and email
+   // check for images
+   // check for avatar
+   // upload in cloudinary,avatar
+   // create user object - create entry in db
+   // remove password and refresh token field
+   // check for user creation
+   // return response
+
+   const {fullName,userName, email, password} = req.body;
+   console.log(fullName,userName, email, password);
+   if([fullName,userName, email, password].some((field)=>field?.trim()==='')){
+    throw new apiError(400,"All Fields are required");
+   }
+   const existedUser= User.findOne({$or:[{userName},{email}]});
+        if(existedUser){
+            throw new apiError(409,"User already exists");
+        }
     
-})
+});
 
 export {registerUser};
