a911f588e3d25f2f44dd5b3c26e1d98eeaa24852
karahmon <monilkarania@gmail.com>
2024-04-17 11:42:56 +0530

    registered user done

--- vidplay/src/controllers/user.controller.js
+++ vidplay/src/controllers/user.controller.js
@@ -1,6 +1,8 @@
 import {asyncHandler} from '../utils/asyncHandler.js';
 import {apiError} from '../utils/apiError.js'
 import {User} from '../models/user.model.js';
+import uploadOnCloudinary, {cloudinary} from '../utils/cloudinary.js';
+import { apiResponse } from '../utils/apiResponse.js';
 
 const registerUser= asyncHandler(async (req, res) => {
    // get user details from frontend
@@ -23,7 +25,32 @@ const registerUser= asyncHandler(async (req, res) => {
         if(existedUser){
             throw new apiError(409,"User already exists");
         }
-    
+
+        const avatarLocalPath=req.files?.avatar[0]?.path;
+        const coverImageLocalPath=req.files?.coverImage[0]?.path;
+
+        if(!avatarLocalPath){
+            throw new apiError(400,"Avatar is required");
+        }
+     const avatar = await uploadOnCloudinary(avatarLocalPath)
+     const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+     if(!avatar){
+        throw new apiError(400,"Avatar is required");
+    }
+    const user= await User.create({
+        fullName,
+        avatar:avatar.url,
+        coverImage:coverImage?.url||"",
+        email,
+        password,
+        userName:userNametoLowerCase(),
+    })
+   const createdUser= await user.findbyId(user._id).select("-password -refreshToken");
+   if(!createdUser){
+    throw new apiError(500,"User not created");
+   }
+   return res.status(201).json(new apiResponse(200,createdUser,"User created"))
 });
 
 export {registerUser};
