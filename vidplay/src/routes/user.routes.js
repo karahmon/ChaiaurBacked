7dd23a10a8766fb0dbe895605d16b8988d273d38
karahmon <monilkarania@gmail.com>
2024-04-24 22:27:33 +0530

    Update user.routes.js to include new routes and update existing routes

--- vidplay/src/routes/user.routes.js
+++ vidplay/src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import {Router} from 'express';
-import { logoutUser, registerUser,loginUser,refreshAccessToken, updateUserAvatar, updateCoverImage } from '../controllers/user.controller.js';
+import { logoutUser, registerUser,loginUser,refreshAccessToken, updateUserAvatar, updateCoverImage, changeCurrentPassword, getCurrentUser, updateUserProfile, getUserChannelProfile, getWatchHistory } from '../controllers/user.controller.js';
 import {upload} from '../middlewares/multer.middleware.js';
 import {verifyJWT} from '../middlewares/auth.middleware.js';
 const router= Router();
@@ -15,7 +15,11 @@ router.route('/login').post(loginUser)
 //secured routes
 router.route('/logout').post(verifyJWT, logoutUser)
 router.route('/refresh-token').post(refreshAccessToken)
-router.route('/update-avatar').post(verifyJWT,upload.fields([{name:"avatar",maxCount:1}]),updateUserAvatar)
-router.route('/update-coverImage').post(verifyJWT,upload.fields([{name:"coverImage",maxCount:1}]),updateCoverImage)
-
+router.route('/change-password').post(verifyJWT,changeCurrentPassword)
+router.route('/current-user').get(verifyJWT,getCurrentUser)
+router.route('/update-profile').patch(verifyJWT,updateUserProfile)
+router.route('/update-avatar').post(verifyJWT,upload.single([{name:"avatar",maxCount:1}]),updateUserAvatar)
+router.route('/update-coverImage').post(verifyJWT,upload.single([{name:"coverImage",maxCount:1}]),updateCoverImage)
+router.route('/c/:username').get(verifyJWT,getUserChannelProfile)
+router.route('/history').get(verifyJWT,getWatchHistory)
 export default router;
